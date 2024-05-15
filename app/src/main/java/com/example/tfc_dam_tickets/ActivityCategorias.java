51b0110e7b3a3140dfdeb2d9fc40c16b1cd063e9
Joana <joanacascogalea@gmail.com>
2024-05-15 16:30:02 +0200

    toolBar activityCategorias

--- app/src/main/java/com/example/tfc_dam_tickets/ActivityCategorias.java
+++ app/src/main/java/com/example/tfc_dam_tickets/ActivityCategorias.java
@@ -1,11 +1,14 @@
 package com.example.tfc_dam_tickets;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.example.tfc_dam_tickets.adapterUtils.AdapterCategorias;
 import com.example.tfc_dam_tickets.model.Category;
 import com.example.tfc_dam_tickets.persistence.CategoryPersistence;
@@ -26,6 +29,7 @@ public class ActivityCategorias extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
+        initializeUI();
 
         // Inicializar RecyclerView
         rvCategorias = findViewById(R.id.rvCategorias);
@@ -53,5 +57,23 @@ public class ActivityCategorias extends AppCompatActivity {
 
         // Configurar RecyclerView con el adaptador
         rvCategorias.setAdapter(adapterCategorias);
+    }
+
+    //TODO MOSTRAR ACTION_BAR
+    private void initializeUI() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_24);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(R.string.tv_titulo_categorias);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // ID del botón de retroceso en la ActionBar
+        if (item.getItemId() == android.R.id.home) {
+            // Finaliza la actividad actual para volver a MainActivity
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
