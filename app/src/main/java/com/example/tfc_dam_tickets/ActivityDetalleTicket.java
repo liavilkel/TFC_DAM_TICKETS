b919a9ed726777af253605077a9d111c70d47e97
Joana <joanacascogalea@gmail.com>
2024-05-15 11:19:35 +0200

    detalleTicket

---
+++ app/src/main/java/com/example/tfc_dam_tickets/ActivityDetalleTicket.java
@@ -0,0 +1,13 @@
+package com.example.tfc_dam_tickets;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDetalleTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_ticket);
+    }
+}
