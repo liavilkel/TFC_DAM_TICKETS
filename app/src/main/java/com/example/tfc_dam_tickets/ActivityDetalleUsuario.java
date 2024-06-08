ef8fe513c1da84bb1248830a6720fd28613b524a
JavierDomi <javi.dominguez.ib@gmail.com>
2024-06-08 17:55:22 +0200

    User info detail

--- app/src/main/java/com/example/tfc_dam_tickets/ActivityDetalleUsuario.java
+++ app/src/main/java/com/example/tfc_dam_tickets/ActivityDetalleUsuario.java
@@ -11,17 +11,89 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tfc_dam_tickets.autenticacion.Login;
+import com.example.tfc_dam_tickets.model.Client;
+import com.example.tfc_dam_tickets.model.User;
+import com.example.tfc_dam_tickets.persistence.ClientPersistence;
+import com.example.tfc_dam_tickets.persistence.UserPersistence;
+import com.example.tfc_dam_tickets.utils.SessionManager;
 
 public class ActivityDetalleUsuario extends AppCompatActivity {
+
+    TextView tvEmail, tvNombre, tvApellidos, tvTelf, tvTipo, tvIdClient, tvClientName,
+            tvClientStreet, tvClientProv, tvClientMun, tvClientZip;
+    Button btnChangePw;
+    UserPersistence userPersistence;
+    ClientPersistence clientPersistence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_usuario);
 
-        //ACTION BAR
+        userPersistence = new UserPersistence(this);
+        clientPersistence = new ClientPersistence(this);
+
+
+        User user = userPersistence.getUserByEmail(SessionManager.getLoggedInUserEmail());
+        Client client = clientPersistence.findClientById(user.getComId());
+
+        initActionBar();
+        initFields();
+        fillInFields(user, client);
+
+        btnChangePw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ActivityDetalleUsuario.this, SetNewPasswordActivity.class);
+                i.putExtra("email", user.getEmail());
+                i.putExtra("outcome", "userInfo");
+                startActivity(i);
+            }
+        });
+
+    }
+
+    private void initFields() {
+        tvEmail = findViewById(R.id.tvDatosEmail);
+        tvNombre = findViewById(R.id.tvDatosNombre);
+        tvApellidos = findViewById(R.id.tvDatosApe);
+        tvTelf = findViewById(R.id.tvDatosTelef);
+        tvTipo = findViewById(R.id.tvDatosTipo);
+
+        tvIdClient = findViewById(R.id.tvDatosIdEmpresa);
+        tvClientName = findViewById(R.id.tvDatosNombreEmp);
+        tvClientStreet = findViewById(R.id.tvDatosCalleEmpresa);
+        tvClientProv = findViewById(R.id.tvDatosProvinciaEmpresa);
+        tvClientMun = findViewById(R.id.tvDatosMunicipioEmpresa);
+        tvClientZip = findViewById(R.id.tvDatosZipEmpresa);
+
+        btnChangePw = findViewById(R.id.btnCambiarPw);
+
+    }
+
+    private void fillInFields(User user, Client client) {
+
+        tvEmail.setText(user.getEmail());
+        tvNombre.setText(user.getName());
+        tvApellidos.setText(user.getLastName());
+        tvTelf.setText(user.getPhoneNum());
+        tvTipo.setText(user.getType());
+
+        tvIdClient.setText(client.getClientId().toString());
+        tvClientName.setText(client.getName());
+        tvClientStreet.setText(client.getStreet());
+        tvClientProv.setText(client.getProvince());
+        tvClientMun.setText(client.getMunicipality());
+        tvClientZip.setText(client.getZipCode());
+
+    }
+
+    private void initActionBar() {
         Toolbar customToolbar = findViewById(R.id.custom_actionbar);
         setSupportActionBar(customToolbar);
         initializeUI();
